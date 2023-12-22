94b189b8d24439299be6f0959ea707f012a3776d
abundance <abundanceanyanwu@gmail.com>
2023-12-22 14:00:32 +0100

    implemented dip721

--- src/propspace_backend/src/lib.rs
+++ src/propspace_backend/src/lib.rs
@@ -1,4 +1,35 @@
+use ic_cdk::export::candid::candid_method;
+use ic_cdk::export::candid::export_service;
+mod env;
+
+#[candid_method]
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[candid_method]
+#[ic_cdk::query]
+fn greet_2(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query(name = "__get_candid_interface_tmp_hack")]
+fn export_candid() -> String {
+    export_service!();
+    __export_service()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::export_candid;
+    #[test]
+    fn save_candid() {
+        use std::env;
+        use std::fs::write;
+        use std::path::PathBuf;
+        let dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let dir = dir.parent().unwrap();
+        write(dir.join("propspace_backend.did"), export_candid()).expect("Write failed.");
+    }
 }
